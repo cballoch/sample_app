961a6d39091b1a0e8b126f9506b84aa4fd7f86e3
Caleb Balloch <caleb.balloch@gmail.com>
2011-12-06 20:29:30 +0800

    Shoot me in the leg finished chapter 3

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,13 +1,19 @@
 class PagesController < ApplicationController
+
   def home
     @title = "Home"
-  end
-
+  end 
+  
   def contact
     @title = "Contact"
   end
-  
+
   def about
     @title = "About"
   end
+  
+  def help
+    @title = "Help"
+  end
+
 end
